950cdbf282a6a66590831eac3c65bedf06b868ce
Sanity.io <hello@sanity.io>
2022-06-10 17:28:02 +0200

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,52 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-gatsby-blog'
+      }
+    },
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '',
+                  title: 'Sanity Studio',
+                  name: 'projectln-07-studio',
+                  apiId: '27884a2c-cdb3-4955-b0fe-aff8bcd4072d'
+                },
+                {
+                  buildHookId: '',
+                  title: 'Blog Website',
+                  name: '',
+                  apiId: ''
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/ArkadiiVodolazskyi/ProjectLN07',
+            category: 'Code'
+          },
+          {title: 'Frontend', value: '', category: 'apps'}
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
