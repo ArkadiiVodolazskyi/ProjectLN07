ed2385efc24300ffa750cb4f4f14f2823ecb9456
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-09-21 23:53:34 +0300

    Improved post card. Added a post.

--- src/components/blog/PostCard.svelte
+++ src/components/blog/PostCard.svelte
@@ -3,17 +3,18 @@
 
   export let post;
   const {
-    book_first_published,
     gradient,
-    cats: categories,
+    title,
     authors
   } = post;
 
   const post_thumbnail_url = post.image ? urlFor(post.image) : null;
 </script>
 
-<div
-  class="post_card"
+<a
+  rel='prefetch'
+  href='/blog/{post.slug.current}'
+  class='post_card'
   style={`
     --grad1: ${gradient.color_1};
     --grad2: ${gradient.color_2};
@@ -21,53 +22,48 @@
   `}
 >
   {#if post_thumbnail_url}
-    <a
-      rel="prefetch"
-      href="/blog/{post.slug.current}"
-      class="thumnail_wrapper"
-    >
-      <div class="backgrop"></div>
-      <img class="tumbnail_img" src="{post_thumbnail_url}" alt="{post.title}">
-    </a>
+    <img class='tumbnail_img' src='{post_thumbnail_url}' alt='{post.title}'>
+  {:else}
+    <h3 class='title_authors'>
+      <p class='title'>{title}</p>
+      <p class='authors'>{authors.map(author => author.name)}</p>
+    </h3>
   {/if}
-</div>
+</a>
 
-<style lang="sass">
+<style lang='sass'>
   .post_card
-    border-radius: calc( var(--radius) + .3rem )
-    position: relative
-    transition: all var(--tr-3)
     box-shadow: var(--shd-1)
-    &:hover .backgrop
-      transform: translate(-50%, -30%)
-    .thumnail_wrapper
-      display: block
-      border-radius: var(--radius)
-      overflow: hidden
-      position: relative
-      z-index: 20
-      padding-top: 100%
-      background-color: hsl(0, 0%, 10%)
-    .backgrop
-      position: absolute
-      z-index: 30
-      top: 50%
-      left: 50%
-      width: 200%
-      height: 200%
-      transform-origin: center
-      transition: all .3s ease
-      transform: translate(-50%, -50%)
-      background: linear-gradient((calc(var(--angle) * 1deg)), var(--grad1) 0%, var(--grad2) 100%)
-    .tumbnail_img
-      position: absolute
-      z-index: 50
-      top: 50%
-      left: 50%
-      transform: translate(-50%, -50%)
-      max-width: 85%
-      max-height: 85%
-      width: auto
-      height: auto
-      box-shadow: 0 0 .3rem .2rem hsla(0, 0%, 13%, .6)
+    display: flex
+    flex-direction: column
+    align-items: center
+    justify-content: center
+    border-radius: var(--radius)
+    overflow: hidden
+    position: relative
+    aspect-ratio: 1
+    background: linear-gradient((calc(var(--angle) * 1deg)), var(--grad1) 0%, var(--grad2) 100%)
+    filter: grayscale(.75)
+    transition: filter .25s ease-in-out
+    &:hover
+      filter: grayscale(0)
+      .tumbnail_img
+        transform: scale(1.02)
+      .title_authors
+        transform: scale(1.08)
+  .tumbnail_img
+    max-width: 85%
+    max-height: 85%
+    width: auto
+    height: auto
+    box-shadow: 0 0 .3rem .2rem hsla(0, 0%, 13%, .6)
+    position: relative
+    z-index: 1
+    transition: transform 1s ease
+  .title_authors
+    padding: 2rem
+    text-align: center
+    transition: transform 1s ease
+    .title
+      font-size: 1.5em
 </style>
