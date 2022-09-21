367c60b9be22037e828b13759c410d50e76d1c66
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-09-21 23:11:14 +0300

    Updated some images.

--- webp.js
+++ webp.js
@@ -3,18 +3,19 @@ import imageminWebp from 'imagemin-webp';
 
 // to process run node webp.js
 
-imagemin(['./src/img/**/*.{jpg,png}'], {
+imagemin(['./src/img-to-webp/**/*.{jpg,jpeg,png}'], {
   destination: './src/img-webp/',
   plugins: [
 		// https://www.npmjs.com/package/@mlz/imagemin-webp
     imageminWebp({
-			quality: 100,
-			method: 6,
-			lossless: true,
+			// quality: 100,
+			// method: 6,
+			// lossless: true,
 			// resize: {
 			// 	width: 420,
 			// 	height: 0
 			// }
+			size: 150000 // bytes
     }),
   ],
 }).then(() => {
