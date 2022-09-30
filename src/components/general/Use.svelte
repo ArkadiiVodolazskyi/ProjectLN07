7dc3d71fd77eff16038efa0190a025373b062790
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-09-30 21:55:12 +0300

    Test #2: SVG's use + resolved_url from Vite.
    Vite docs: https://vitejs.dev/guide/assets.html#new-url-url-import-meta-url

--- src/components/general/Use.svelte
+++ src/components/general/Use.svelte
@@ -1,7 +1,6 @@
 <script>
 	export let type = '';
-	// const resolved_url = new URL('./icons.svg', import.meta.url).href;
-	const resolved_url = '/icons.svg';
+	const resolved_url = new URL('./icons.svg', import.meta.url).href;
 </script>
 
 <use xlink:href={`${resolved_url}#${type}`}></use>
