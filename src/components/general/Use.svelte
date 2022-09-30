f1cbe124754181f1207d8a94a17d61f46b3c4914
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-09-30 22:11:07 +0300

    Test #3: SVG's use + native href.
    MDN docs (deprecated xlink:href): https://developer.mozilla.org/en-US/docs/Web/SVG/Element/use

--- src/components/general/Use.svelte
+++ src/components/general/Use.svelte
@@ -1,6 +1,6 @@
 <script>
+	import icons from '$icons';
 	export let type = '';
-	const resolved_url = new URL('./icons.svg', import.meta.url).href;
 </script>
 
-<use xlink:href={`${resolved_url}#${type}`}></use>
+<use href={icons + `#${type}`}></use>
